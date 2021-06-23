353d6eb8a2e675871b5a4b2c96015a17a4d2e063
Sechaba Mokobane <sechaba.mokobane@gmail.com>
2021-06-23 21:40:06 +0200

    Completed the party invite exercise

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-#The following is code that helps develop an understanding of the basics of opening, reading, and writing to files using Python. 
+#The following is code that helps develop an understanding of the basics of opening, reading, and writing to files using Python. g
--- main.py
+++ main.py
@@ -6,3 +6,21 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open('Input/Letters/starting_letter.txt') as letter:
+    letter = letter.read()
+
+with open('Input/Names/invited_names.txt') as names:
+    names = names.readlines()
+
+for name in names:
+    stripped_name = name[:-1]
+    new_letter = letter.replace('[name]',stripped_name)
+    new_letter = new_letter.replace('Angela','Sechaba')
+    with open(f'letter_for_{stripped_name}.txt','w') as output_letter:
+        output_letter.write(new_letter)
+    
+    
+
+
+  
